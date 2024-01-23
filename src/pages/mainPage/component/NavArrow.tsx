32ee6989f2915d19c0e3465e42fc9a86034d7467
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2024-01-23 18:37:40 +0200

    fix: navArrow scroll

--- src/pages/mainPage/component/NavArrow.tsx
+++ src/pages/mainPage/component/NavArrow.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 const sections = ['#section-0', '#section-1', '#section-2'];
+
+export function isVisible(elem: HTMLElement) {
+  const coords = elem.getBoundingClientRect();
+  const windowHeight = document.documentElement.clientHeight;
+  const topVisible = coords.top > 0 && coords.top < windowHeight;
+  const bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+  return topVisible || bottomVisible;
+}
 
 const StyledNavArrow = styled('a')`
   position: fixed;
@@ -14,19 +22,48 @@ const StyledNavArrow = styled('a')`
 
 export function NavArrow() {
   const location = useLocation();
+
+  const [istItBottom, setItIsBottom] = useState(false);
+
   const index =
     sections.indexOf(location.hash) === -1
       ? 0
       : sections.indexOf(location.hash);
 
-  const isItLast = index + 1 === sections.length;
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (location.hash === '#section-2') {
+      setItIsBottom(true);
+    } else {
+      setItIsBottom(false);
+    }
+  }, [location.hash]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const elementPosition =
+        (ref.current as unknown as HTMLAnchorElement).getBoundingClientRect()
+          .top + window.scrollY;
+      const lowestPosition = document.body.clientHeight - window.innerHeight;
+
+      if (elementPosition > lowestPosition) {
+        setItIsBottom(true);
+      } else {
+        setItIsBottom(false);
+      }
+    };
+    handleScroll();
+    document.addEventListener('scrollend', handleScroll);
+
+    return () => {
+      document.removeEventListener('scrollend', handleScroll);
+    };
+  }, []);
 
   return (
-    <StyledNavArrow
-      href={sections[isItLast ? 0 : index + 1]}
-      onClick={() => {}}
-    >
-      {isItLast ? (
+    <StyledNavArrow href={sections[istItBottom ? 0 : index + 1]} ref={ref}>
+      {istItBottom ? (
         <i className="fa-solid fa-arrow-up" />
       ) : (
         <i className="fa-solid fa-arrow-down" />
