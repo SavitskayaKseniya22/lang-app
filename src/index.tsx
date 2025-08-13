d880fb1eb7ede265a0c189fea30cb6dc18360fad
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2025-08-14 01:42:04 +0300

    fix: add tg funcs

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { init, backButton } from '@telegram-apps/sdk-react';
 import App from './App';
 import Spinner from './components/spinner/Spinner';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 
+
+
 import { store } from './store/store';
+
+init();
+
+// Mount the Back Button, so we will work with 
+// the actual component properties.
+backButton.mount();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
